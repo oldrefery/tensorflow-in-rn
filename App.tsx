4f922b79c00c9dc024040e37cc10035b4539ad0d
Rustem.Shakirov <Rustem.Shakirov@emea.merkleinc.com>
2023-10-18 20:24:58 +0200

    feat: add flow for processing image with AI predefined models and display results

--- App.tsx
+++ App.tsx
@@ -4,13 +4,23 @@ import { styles } from "./App.styles";
 import { useState } from "react";
 import { Button } from "./src/components/Button";
 import * as ImagePicker from "expo-image-picker";
+import * as tensorflow from "@tensorflow/tfjs";
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import * as FileSystem from "expo-file-system";
+import { decodeJpeg } from "@tensorflow/tfjs-react-native";
+import {
+  Classification,
+  ClassificationProps,
+} from "./src/components/Classification";
 
 export default function App() {
   const [selectedImageUri, setSelectedImageUri] = useState("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [results, setResults] = useState<ClassificationProps[]>([]);
 
   const handleSelectImage = async () => {
     setIsLoading(true);
+    setResults([]);
 
     try {
       const result = await ImagePicker.launchImageLibraryAsync({
@@ -19,13 +29,33 @@ export default function App() {
       });
 
       if (!result.canceled) {
-        setSelectedImageUri(result.assets[0].uri);
+        const { uri } = result.assets[0];
+        setSelectedImageUri(uri);
+        await imageClassification(uri);
       }
     } catch (e) {
       console.log("Error", e);
     } finally {
       setIsLoading(false);
     }
+  };
+
+  const imageClassification = async (imageUri: string) => {
+    await tensorflow.ready();
+    const model = await mobilenet.load();
+
+    const imageBase64 = await FileSystem.readAsStringAsync(imageUri, {
+      encoding: FileSystem.EncodingType.Base64,
+    });
+
+    const imgBuffer = tensorflow.util.encodeString(
+      imageBase64,
+      "base64"
+    ).buffer;
+    const raw = new Uint8Array(imgBuffer);
+    const imageTensor = decodeJpeg(raw);
+    const classificationResult = await model.classify(imageTensor);
+    setResults(classificationResult);
   };
 
   return (
@@ -39,7 +69,11 @@ export default function App() {
         }}
         style={styles.image}
       />
-      <View style={styles.resultContainer}></View>
+      <View style={styles.resultContainer}>
+        {results.map((result) => (
+          <Classification data={result} key={result.className} />
+        ))}
+      </View>
       {isLoading ? (
         <ActivityIndicator color={"#5F1BBF"} />
       ) : (
